7d7bf1e1637a7df3d1498d0ec11bf2cffc5d4325
Alexey Abakhtimov <abahtimov2014@gmail.com>
2020-05-27 18:09:55 +0300

    Added new practice work
    Databases practice work added (03)

---
+++ Practice_Work_03/Practice_Work_03/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Practice_Work_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.generateData_button = new System.Windows.Forms.Button();
+            this.rowNumber_textBox = new System.Windows.Forms.TextBox();
+            this.columnNumber_textBox = new System.Windows.Forms.TextBox();
+            this.rowNumber_label = new System.Windows.Forms.Label();
+            this.columnNumber_label = new System.Windows.Forms.Label();
+            this.error_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(1013, 543);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // generateData_button
+            // 
+            this.generateData_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateData_button.Location = new System.Drawing.Point(541, 666);
+            this.generateData_button.Name = "generateData_button";
+            this.generateData_button.Size = new System.Drawing.Size(222, 75);
+            this.generateData_button.TabIndex = 1;
+            this.generateData_button.Text = "Сгенерирвать таблицу!";
+            this.generateData_button.UseVisualStyleBackColor = true;
+            this.generateData_button.Click += new System.EventHandler(this.generateData_button_Click);
+            // 
+            // rowNumber_textBox
+            // 
+            this.rowNumber_textBox.Location = new System.Drawing.Point(309, 596);
+            this.rowNumber_textBox.Name = "rowNumber_textBox";
+            this.rowNumber_textBox.Size = new System.Drawing.Size(176, 26);
+            this.rowNumber_textBox.TabIndex = 2;
+            // 
+            // columnNumber_textBox
+            // 
+            this.columnNumber_textBox.Location = new System.Drawing.Point(309, 675);
+            this.columnNumber_textBox.Name = "columnNumber_textBox";
+            this.columnNumber_textBox.Size = new System.Drawing.Size(176, 26);
+            this.columnNumber_textBox.TabIndex = 3;
+            // 
+            // rowNumber_label
+            // 
+            this.rowNumber_label.AutoSize = true;
+            this.rowNumber_label.Location = new System.Drawing.Point(319, 637);
+            this.rowNumber_label.Name = "rowNumber_label";
+            this.rowNumber_label.Size = new System.Drawing.Size(147, 20);
+            this.rowNumber_label.TabIndex = 4;
+            this.rowNumber_label.Text = "Количество строк";
+            // 
+            // columnNumber_label
+            // 
+            this.columnNumber_label.AutoSize = true;
+            this.columnNumber_label.Location = new System.Drawing.Point(319, 721);
+            this.columnNumber_label.Name = "columnNumber_label";
+            this.columnNumber_label.Size = new System.Drawing.Size(166, 20);
+            this.columnNumber_label.TabIndex = 5;
+            this.columnNumber_label.Text = "Количество колонок";
+            // 
+            // error_label
+            // 
+            this.error_label.AutoSize = true;
+            this.error_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.error_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.error_label.Location = new System.Drawing.Point(541, 596);
+            this.error_label.Name = "error_label";
+            this.error_label.Size = new System.Drawing.Size(0, 29);
+            this.error_label.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 774);
+            this.Controls.Add(this.error_label);
+            this.Controls.Add(this.columnNumber_label);
+            this.Controls.Add(this.rowNumber_label);
+            this.Controls.Add(this.columnNumber_textBox);
+            this.Controls.Add(this.rowNumber_textBox);
+            this.Controls.Add(this.generateData_button);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Генератор таблиц";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button generateData_button;
+        private System.Windows.Forms.TextBox rowNumber_textBox;
+        private System.Windows.Forms.TextBox columnNumber_textBox;
+        private System.Windows.Forms.Label rowNumber_label;
+        private System.Windows.Forms.Label columnNumber_label;
+        private System.Windows.Forms.Label error_label;
+    }
+}
+
---
+++ Practice_Work_03/Practice_Work_03/Form1.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Абахтимов Алексей Александрович. Группа 191-311 (Управление в технических системах, 1 курс).
+ * Московский Политехнический университет.
+ * Ссылка на GitHub: https://github.com/AmazingRoovy
+ * Дата и время последнего редактирования: 19:50 22.05.2020.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_Work_03
+{
+    public partial class Form1 : Form
+    {
+        public const int NUM_DEFAULT_ROWS    = 3;
+        public const int NUM_DEFAULT_COLUMNS = 3;
+
+        public Form1()
+        { 
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Columns.Clear();
+            dataGridView.Refresh();
+
+            DataGridViewTextBoxColumn col;
+            for (int c = 0; c < NUM_DEFAULT_COLUMNS; ++c)
+            {
+                col = new DataGridViewTextBoxColumn();
+                col.HeaderText = "Column " + (c + 1).ToString();
+                col.ReadOnly = true;
+                col.Width = 125;
+
+                dataGridView.Columns.Add(col);
+            }
+
+            Random random = new Random();
+            DataGridViewRow row;
+            DataGridViewTextBoxCell cell;
+            for (int r = 0; r < NUM_DEFAULT_ROWS; ++r)
+            {
+                row = new DataGridViewRow();
+                for (int c = 0; c < NUM_DEFAULT_COLUMNS; ++c)
+                {
+                    cell = new DataGridViewTextBoxCell();
+                    // Отображение в бежевом - лучшее качество просмотра.
+                    cell.Style.BackColor = Color.Beige;
+
+                    cell.Style.ForeColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256), random.Next(256));
+                    cell.Value = random.Next(101).ToString();
+                    row.Cells.Add(cell);
+                }
+                dataGridView.Rows.Add(row);
+            }
+        }
+
+        private void generateData_button_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Columns.Clear();
+            dataGridView.Refresh();
+
+            this.error_label.Text = "";
+
+            int rowNumber;
+            int columnNumber;
+
+            if (this.rowNumber_textBox.Text == "")
+                rowNumber_textBox.Text = NUM_DEFAULT_ROWS.ToString();
+            if (this.columnNumber_textBox.Text == "")
+                columnNumber_textBox.Text = NUM_DEFAULT_COLUMNS.ToString();
+
+            if (!int.TryParse(this.rowNumber_textBox.Text.Trim(), out rowNumber))
+            {
+                this.error_label.Text = "Неправильный формат!";
+                rowNumber = NUM_DEFAULT_ROWS;
+            }
+
+            if (!int.TryParse(this.columnNumber_textBox.Text.Trim(), out columnNumber))
+            {
+                this.error_label.Text = "Неправильный формат!";
+                columnNumber = NUM_DEFAULT_COLUMNS;
+            }
+
+            // Ограничение по оптимальному количеству корректно добавляемых колонок в DataGridView. 
+            if (columnNumber > 654)
+            {
+                columnNumber = 654;
+                columnNumber_textBox.Text = columnNumber.ToString();
+            }
+
+            Random random = new Random();
+            DataGridViewTextBoxColumn col;
+            for (int c = 0; c < columnNumber; ++c)
+            {
+                col = new DataGridViewTextBoxColumn();
+                col.HeaderText = "Column " + (c + 1).ToString();
+                col.ReadOnly = true;
+                col.Width = 125;
+
+                dataGridView.Columns.Add(col);
+            }
+
+            DataGridViewRow row;
+            DataGridViewTextBoxCell cell;
+            for (int r = 0; r < rowNumber; ++r)
+            {
+                row = new DataGridViewRow();
+                for (int c = 0; c < columnNumber; ++c)
+                {
+                    cell = new DataGridViewTextBoxCell();
+                    // Отображение в бежевом - лучшее качество просмотра.
+                    cell.Style.BackColor = Color.Beige;
+
+                    cell.Style.ForeColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256), random.Next(256));
+                    cell.Value = random.Next(101).ToString();
+                    row.Cells.Add(cell);
+                }
+                dataGridView.Rows.Add(row);
+            }
+            
+        }
+    }
+}
